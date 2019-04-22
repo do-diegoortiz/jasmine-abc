39ef373b4baffa26e96dd90703cc96ec34248191
Diego Ortiz <diego.ortiz@impartner.com>
2019-04-22 17:06:39 -0500

    Initial commit, basic UI and Jasmine implementation with the basic files and a title test

---
+++ index.js
@@ -0,0 +1,43 @@
+function calculate(inputVal) {
+    const regEx = /\+|\-|\*|\//;
+    const numbers = inputVal.split(regEx);
+    [numberA, numberB] = [parseInt(numbers[0]), parseInt(numbers[1])];
+    const operator = inputVal.match(regEx);
+
+    if(Number.isNaN(numberA) || Number.isNaN(numberB) || operator === null) {
+        updateResult("Operation not valid")
+        return
+    }
+
+    const calculator = new Calculator();
+
+    calculator.add(numberA);
+
+    let result = 0
+
+    switch(operator[0]) {
+        case '+':
+            result = calculator.add(numberB)
+            break;
+        case '-':
+            result = calculator.substract(numberB)
+            break;
+        case '*':
+            result = calculator.multiply(numberB)
+            break;
+        case '/':
+            result = calculator.divide(numberB)
+            break;
+    }
+
+    updateResult(result)
+}
+
+function updateResult(result) {
+    let element = document.getElementById("result")
+
+    if(element) {
+        element.innerHTML= result;
+    }
+    
+}
