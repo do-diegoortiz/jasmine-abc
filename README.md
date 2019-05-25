feb75ba8be5c8be811bbdf15843b45dffde93282
Diego Ortiz <alejojo23@gmail.com>
2019-05-25 10:32:14 -0500

    Expectations set for every calculator method. Video 17

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
-### IT
-'**it**' is a Global function (we don't need to import anything)
+## Global Methods
+Methods that we don't need to import in order to use them.
+
+### it
 Receives two parameters:
 1) The title of the expectation
 2) The assertion, in the form of an anonymous function
 TIP: To read the title of the spec, include the word "it" as part of the sentence
 
+### expect
+Return true or false. It has two parts:
+1) Actual value, or main parameter of the expect
+2) Chained with a matcher using dot (.)
+e.g. expect(calculator.value).toBe(5)
 
 'spec' is short for specification
--- calculator.spec.js
+++ calculator.spec.js
@@ -1,14 +1,29 @@
 describe('calculator.js', function (){
     it('should add numbers to the total', () => {
+        const calculator = new Calculator();
+        calculator.add(5);
 
+        expect(calculator.total).toBe(5);
     })
     it('should substract number from total', () => {
+        const calculator = new Calculator();
+        calculator.total = 30;
+        calculator.substract(5);
 
+        expect(calculator.total).toBe(25);
     })
     it('should multiply number by total', () => {
+        const calculator = new Calculator();
+        calculator.total = 23;
+        calculator.multiply(4);
 
+        expect(calculator.total).toBe(92);
     })
     it('should divide number by total', () => {
+        const calculator = new Calculator();
+        calculator.total = 60;
+        calculator.divide(6);
 
+        expect(calculator.total).toBe(10);
     })
 });
